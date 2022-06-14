067f7b8572518858eac7b9ed5df3b431a5d60aaf
Kacper Sokol <ks1591@my.bristol.ac.uk>
2022-06-14 14:55:12 +1000

    Execution environment configuration infrastructure

--- xml_book/__init__.py
+++ xml_book/__init__.py
@@ -8,9 +8,13 @@ This library implements a collection of Python modules used by the book.
 # Author: Kacper Sokol <kacper@xmlx.io>
 # License: MIT
 
+import xml_book.config as cfg
+
 __author__ = 'Kacper Sokol'
 __email__ = 'kacper@xmlx.io'
 __license__ = 'MIT'
 __version__ = '0.1'
 
 __all__ = []
+
+cfg.setup_plotting()
---
+++ xml_book/config.py
@@ -0,0 +1,21 @@
+"""
+Set Up Execution Environment
+============================
+
+This module configures the book execution environment.
+These options are loaded and initialised in the module init.
+"""
+
+# Author: Kacper Sokol <kacper@xmlx.io>
+# License: MIT
+
+import matplotlib.pyplot as plt
+
+from IPython.display import set_matplotlib_formats
+
+__all__ = ['setup_plotting']
+
+def setup_plotting():
+    """Configures default plotting settings."""
+    plt.style.use('seaborn')
+    set_matplotlib_formats('svg')
